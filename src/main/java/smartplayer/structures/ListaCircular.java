0610b83a589936d92a809133ad0fb7abd4d5c18a
rmari <rmari@Mariz_Reyes>
2026-05-23 00:19:11 -0600

    VerListCircu 1.2

--- src/main/java/smartplayer/structures/ListaCircular.java
+++ src/main/java/smartplayer/structures/ListaCircular.java
@@ -8,6 +8,31 @@ package smartplayer.structures;
  *
  * @author rmari
  */
-public class ListaCircular {
-    
+public class ListaCircular <T> implements Iterable <T> {
+     
+    private static class Nodo <T> {
+        T dato;
+        Nodo<T> siguiente;
+        Nodo(T dato) { this.dato = dato; }
+    }
+
+    private Nodo<T> cola;   // cola.siguiente = cabeza
+    private Nodo<T> actual; // puntero de reproducción
+    private int tamano;
+
+    //  Insercion
+
+    public void agregar(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (cola == null) {
+            nuevo.siguiente = nuevo;   // apunta a si mismo
+            cola = nuevo;
+        } else {
+            nuevo.siguiente = cola.siguiente;
+            cola.siguiente  = nuevo;
+            cola = nuevo;
+        }
+        if (actual == null) actual = cola.siguiente; // cabeza
+        tamano++;
+    }
 }
