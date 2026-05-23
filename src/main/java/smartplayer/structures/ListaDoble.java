12c74aeb856f9d27ec5308bea37275f60cd771d2
rmari <rmari@Mariz_Reyes>
2026-05-22 23:26:36 -0600

    VerListDobl 1.2

--- src/main/java/smartplayer/structures/ListaDoble.java
+++ src/main/java/smartplayer/structures/ListaDoble.java
@@ -8,6 +8,44 @@ package smartplayer.structures;
  *
  * @author rmari
  */
-public class ListaDoble {
+  public class ListaDoble<T> implements Iterable<T> {
+
+    // Nodo interno 
+    public static class Nodo<T> {
+        public T dato;
+        public Nodo<T> siguiente;
+        public Nodo<T> anterior;
+        Nodo(T dato) { this.dato = dato; }
+    }
+
+    private Nodo<T> cabeza;
+    private Nodo<T> cola;
+    private Nodo<T> actual;   // puntero de navegacion 
+    private int tamano;
     
-}
+      // Insercion 
+
+    public void agregarInicio(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (cabeza == null) { cabeza = cola = nuevo; }
+        else {
+            nuevo.siguiente = cabeza;
+            cabeza.anterior = nuevo;
+            cabeza = nuevo;
+        }
+        if (actual == null) actual = cabeza;
+        tamano++;
+    }
+
+    public void agregarFinal(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (cola == null) { cabeza = cola = nuevo; }
+        else {
+            cola.siguiente = nuevo;
+            nuevo.anterior = cola;
+            cola = nuevo;
+        }
+        if (actual == null) actual = cabeza;
+        tamano++;
+    }
+  } 
