dd3bcf5990fbe01c25252946bdf1123366e63318
rmari <rmari@Mariz_Reyes>
2026-05-23 00:51:31 -0600

    VerListCol 1.2

--- src/main/java/smartplayer/structures/Cola.java
+++ src/main/java/smartplayer/structures/Cola.java
@@ -4,10 +4,40 @@
  */
 package smartplayer.structures;
 
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author rmari
  */
-public class Cola {
+public class Cola <T> {
     
+
+    private static class Nodo<T> {
+        T dato;
+        Nodo<T> siguiente;
+        Nodo(T dato) { this.dato = dato; }
+    }
+
+    private Nodo<T> frente;
+    private Nodo<T> final_;
+    private int tamano;
+
+    // Encola un elemento al final 
+    public void enqueue(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (final_ == null) { frente = final_ = nuevo; }
+        else { final_.siguiente = nuevo; final_ = nuevo; }
+        tamano++;
+    }
+
+    // Desencola el elemento del frente 
+    public T dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Cola vacía");
+        T dato = frente.dato;
+        frente = frente.siguiente;
+        if (frente == null) final_ = null;
+        tamano--;
+        return dato;
+    }
 }
