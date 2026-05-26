f25bdfcc861c05e4044639806922c1576fe52f70
rmari <rmari@Mariz_Reyes>
2026-05-25 22:40:32 -0600

    VerGesto 1.1

--- src/main/java/smartplayer/player/GestorPlaylist.java
+++ src/main/java/smartplayer/player/GestorPlaylist.java
@@ -4,10 +4,38 @@
  */
 package smartplayer.player;
 
+import smartplayer.model.Cancion;
+import smartplayer.model.Playlist;
+import smartplayer.structures.ListaSimple;
+
 /**
  *
  * @author rmari
  */
 public class GestorPlaylist {
+    
+     private final ListaSimple<Playlist> playlists = new ListaSimple<>();
+
+    // CRUD 
+    public Playlist crearPlaylist(String nombre) {
+        Playlist p = new Playlist(nombre);
+        playlists.agregarFinal(p);
+        return p;
+    }
+
+    public boolean eliminarPlaylist(String nombre) {
+        Playlist p = buscarPlaylist(nombre);
+        if (p == null) 
+         return false;
+        return playlists.eliminar(p);
+    }
+
+    public boolean agregarCancion(String nombrePlaylist, Cancion c) {
+        Playlist p = buscarPlaylist(nombrePlaylist);
+        if (p == null) 
+            return false;
+        p.agregarCancion(c);
+        return true;
+    }
     
 }
