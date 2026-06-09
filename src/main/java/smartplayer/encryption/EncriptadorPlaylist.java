6174afe5b38d560ffa0d092b993fc88e0acc34b1
rmari <rmari@Mariz_Reyes>
2026-06-08 21:47:22 -0600

    VerEncri 1.2

--- src/main/java/smartplayer/encryption/EncriptadorPlaylist.java
+++ src/main/java/smartplayer/encryption/EncriptadorPlaylist.java
@@ -28,34 +28,70 @@ public class EncriptadorPlaylist {
      * @param recorrido  tipo de recorrido para generar la clave
      * @return texto cifrado (o descifrado)
      */
-    public static String encriptar(String contenido, Playlist playlist, TipoRecorrido recorrido) {
+     
+     public static String encriptar(String contenido, Playlist playlist, TipoRecorrido recorrido) {
         String clave = generarClave(playlist, recorrido);
         return xorConClave(contenido, clave);
     }
     
-  private static String generarClave(Playlist playlist, TipoRecorrido recorrido) {
+     private static String generarClave(Playlist playlist, TipoRecorrido recorrido) {
+     ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda();
 
-    ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda();
-
-    for (Cancion c : playlist.getCanciones()) {abb.insertar(c);
+     for (Cancion c : playlist.getCanciones()) {abb.insertar(c);
     }
 
-    List<Cancion> orden;
-    if (recorrido == TipoRecorrido.PRE_ORDEN) {
-        orden = abb.preOrden();
+     List<Cancion> orden;
+     if (recorrido == TipoRecorrido.PRE_ORDEN) {orden = abb.preOrden();
     }else if (recorrido == TipoRecorrido.POST_ORDEN) {
         orden = abb.postOrden();
     }else{
         orden = abb.inOrden();
     }
-
     StringBuilder sb = new StringBuilder();
 
-    for (Cancion c : orden) {
+     for (Cancion c : orden) {
         sb.append(c.getNombre());
         sb.append(c.getArtista());
     }
-    return sb.toString();
-   }
-   
+     return sb.toString();
+    }
+     private static String xorConClave(String texto, String clave) {
+
+      if (clave.isEmpty()) {
+        return texto;
+    }
+  
+     char[] tc = texto.toCharArray();
+     char[] cc = clave.toCharArray();
+     char[] resultado = new char[tc.length];
+
+     for (int i = 0; i < tc.length; i++) {
+        char letraTexto = tc[i];
+        char letraClave = cc[i % cc.length];
+        resultado[i] = (char) (letraTexto ^ letraClave);
+    }
+
+     return new String(resultado);
+    }
+     
+     public static String exportarPlaylist(Playlist playlist) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#SMARTPLAYER_PLAYLIST\n");
+        sb.append("NOMBRE:").append(playlist.getNombre()).append("\n");
+        sb.append("ID:").append(playlist.getId()).append("\n");
+        sb.append("CANCIONES:\n");
+        for (Cancion c : playlist.getCanciones()) {
+            sb.append(c.getNombre()).append("|")
+              .append(c.getArtista()).append("|")
+              .append(c.getAlbum()).append("|")
+              .append(c.getGenero()).append("|")
+              .append(c.getDuracion()).append("|")
+              .append(c.getTamano()).append("|")
+              .append(c.getRuta()).append("|")
+              .append(c.getAnio()).append("\n");
+        }
+        return sb.toString();
+    }
+     
+     
 }
